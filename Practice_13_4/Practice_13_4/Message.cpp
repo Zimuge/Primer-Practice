6392dbb7b22fbec609ad3e9a1adfee099e115798
Zimuge <790384392@qq.com>
2016-04-19 22:52:46 +0800

    commit the Practice_13_4

---
+++ Practice_13_4/Practice_13_4/Message.cpp
@@ -0,0 +1,38 @@
+#include "stdafx.h"
+#include "Message.h"
+
+/********************** constructor *********************/
+ Message::Message(const std::string & str = "") : contents(str)
+{
+
+}
+
+Message::Message(const Message & msg)
+{
+
+}
+/***********************member function*******************/
+
+// function use to save message to a folder
+void Message::save(Folder & fd)
+{
+	this->folders.insert(&fd);
+}
+
+// function use to remove message from a folder
+void Message::remove(Folder & fd)
+{
+
+}
+
+/*********************** operator *************************/
+Message & Message::operator =(const Message & msg)
+{
+	return *this;
+}
+
+
+/*********************** deconstructor *******************/
+Message::~Message()
+{
+}
